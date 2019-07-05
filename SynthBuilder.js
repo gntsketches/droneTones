5d4751ee65e11be8fedf5a1aa642f7f5987bd1b0
Greg <gntsketches@gmail.com>
2019-07-04 22:05:01 -0700

    added base pitch select feature. adjusted SynthBuilder. cleaned up a bit.

--- SynthBuilder.js
+++ SynthBuilder.js
@@ -1,29 +1,32 @@
 // 
 
 function SynthBuilder() {
-	const synthTypes = [sine]//, triangle, sawtooth]//, square]
-	
-	// 	this._autoFilter = new Tone.AutoFilter("4n").start();
-	//this._tremolo = new Tone.Tremolo().start();	
-	//this._phaser = new Tone.Phaser(); 
-	
-	//this._synth = new Tone.Synth(synthTypes[Math.floor(Math.random()*synthTypes.length)])
-	this._synth = new Tone.Synth(new PartialBuilder(30, 30, 5, 0.1))
-	
+	const synthTypes = [sine, triangle, sawtooth]//, square]
+
+	if (Math.random()<0.4) {
+		this._synth = new Tone.Synth(synthTypes[Math.floor(Math.random() * synthTypes.length)])
+	} else {
+		this._synth = new Tone.Synth(new PartialBuilder(30, 30, 5, 0.1))
+	}
+
+	//this._autoFilter = new Tone.AutoFilter("4n").start();
+	//this._tremolo = new Tone.Tremolo().start();
+	//this._phaser = new Tone.Phaser();
+
 	this._chorus = new Tone.Chorus()
 	  this._chorus.delayTime = Math.random() * 2 + 0.1 // in ms,3.5 ,usually between 2 and 20 // higher values do cool stuff!
 		this._chorus.depth = Math.random() / 2 + 0.5 // normal range, 0.7 
 		this._chorus.frequency.value = Math.random() * 2  // of LFO, 1.5
 		this._chorus.spread = Math.random() * 90 + 90 // degrees panning, 180
 		// .type
-		this._chorus.wet.value = 0//Math.random() / 2 + 0.5// normal range 
+		this._chorus.wet.value = Math.random() //  /2 + 0.5// normal range
 		
 	this._vibrato = new Tone.Vibrato()
 		// this._vibrato.depth.value = Math.random() // normal range, 0.1, is fine.
 		//this._vibrato.frequency.value = Math.random() * 5 // frequency, 5
 		// this._vibrato.maxDelay = Math.random() // 0.005,
 		// .type
-		this._vibrato.wet = 0//Math.random() // 2 + 0.5 // normal range 
+		this._vibrato.wet = Math.random() // /2 + 0.5 // normal range
 
 	this._synth.connect(this._chorus)
 	this._chorus.connect(this._vibrato)
--- main.js
+++ main.js
@@ -4,8 +4,9 @@ var synths = []
 var counter = 0
 var rests = 0 //3
 var tempo = 15
-var pitches = ['g1', 'g2', 'g2', 'd3']
-
+// var pitches = ['g1', 'g2', 'g2', 'd3']
+var basePitch = 'g1'
+var detunings = [0, 1200, 1200, 1900]
 
 
 // main 
@@ -19,7 +20,7 @@ function addSynth() {
 function shiftSynths() {
 	synths[0].dispose() // does this work? doesn't seem like it.
 	synths.shift()
-	addSynth()};
+	addSynth()}
 
 
 var loop = new Tone.Loop(function (time) {
@@ -30,7 +31,8 @@ var loop = new Tone.Loop(function (time) {
 		shiftSynths()
 	}
 	if (beat < synths.length) { 
-		synths[beat].triggerAttackRelease(pitches[beat], 8)	
+		synths[beat].detune.value = detunings[beat] // 400
+		synths[beat].triggerAttackRelease(basePitch,8) // pitches[beat], 8)
 		console.log(synths[beat].oscillator.type)
 	}
 	counter++
@@ -46,18 +48,22 @@ for (var i=0; i<4; i++) {
 Tone.Transport.bpm.value = tempo
 Tone.Transport.start();
 
-var synth = new Tone.Synth()
-
 // UI
 
 document.querySelector('#start').addEventListener('click', () => { 
 	//synth.triggerAttackRelease('c4', '16n')
 	Tone.context.resume()
 	loop.start(0)
-
 })
+
 document.querySelector('#stop').addEventListener('click', () => { 
 	loop.stop()
-
 })
+
+document.querySelector('#base_pitch').addEventListener('change', (e) => {
+	console.log(e.target.value)
+	basePitch = e.target.value
+})
+// https://2ality.com/2012/08/ids-are-global.html
+// https://stackoverflow.com/questions/3434278/do-dom-tree-elements-with-ids-become-global-variables
 
